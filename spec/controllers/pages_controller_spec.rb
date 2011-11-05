4ab116029334c353fc6e0ec40e5ffecb8359e57d
Kazmasaurus <kazmasaurus@gmail.com>
2011-11-05 02:32:51 -0700

    Added test for title on 'about', 'home', and 'contact'

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,6 +8,13 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", 
+          :content => "Magicland | Home", )
+    end
+
   end
 
   describe "GET 'contact'" do
@@ -15,12 +22,25 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", 
+          :content => "Magicland | Contact", )
+    end
+
   end
   
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", 
+          :content => "Magicland | About", )
     end
 
   end
